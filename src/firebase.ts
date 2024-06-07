53444a11d988d83191910c1f0d5d3b1606e3e01b
Alejandro Estrella <aqealejandro@gmail.com>
2024-06-07 01:38:03 -0700

    fixed firebase setup for heroku

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,45 +12,24 @@ function getFirebaseAdmin() {
 
     const getFirebaseApp = (): admin.app.App => {
         if(app == undefined) { //this get reruns on every change durring preview, but firebase admin still sees the pervious instance made, so this just checks if we can use a previous firebase instance, otherwise it will cause an error because firebase thinks we are reintializing
-            if(process.env.DEV == "TRUE") {
-                if(admin.apps == null) {
-                    app = admin.initializeApp({
-                        credential: (admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN as string) as admin.ServiceAccount))
-                    }, "Server");
-                } else if(admin.apps.length == 0) {
-                    app = admin.initializeApp({
-                        credential: (admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN as string) as admin.ServiceAccount))
-                    }, "Server");
-                } else {
-                    var found = false;
-                    for(var i = 0; i < admin.apps.length; i++) {
-                        if(admin.apps[i] != null && (admin.apps[i] as admin.app.App).name == "Server") {
-                            app = admin.apps[i] as admin.app.App;
-                            found = true;
-                        }
-                    }
-                    if(found == false) {
-                        throw new Error("Firebase Admin is being goofy again");
+            if(admin.apps == null) {
+                app = admin.initializeApp({
+                    credential: (admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN as string) as admin.ServiceAccount))
+                }, "Server");
+            } else if(admin.apps.length == 0) {
+                app = admin.initializeApp({
+                    credential: (admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN as string) as admin.ServiceAccount))
+                }, "Server");
+            } else {
+                var found = false;
+                for(var i = 0; i < admin.apps.length; i++) {
+                    if(admin.apps[i] != null && (admin.apps[i] as admin.app.App).name == "Server") {
+                        app = admin.apps[i] as admin.app.App;
+                        found = true;
                     }
                 }
-            } else {
-                if(admin.apps == null) {
-                    app = admin.initializeApp({}, "Server");
-                } else if(admin.apps.length == 0) {
-                    app = admin.initializeApp({}, "Server");
-                } else {
-                    var found = false;
-                    
-                    for(var i = 0; i < admin.apps.length; i++) {
-                        if(admin.apps[i] != null && (admin.apps[i] as admin.app.App).name == "Server") {
-                            app = admin.apps[i] as admin.app.App;
-                            found = true;
-                        }
-                    }
-
-                    if(found == false) {
-                        throw new Error("Firebase Admin is being goofy again");
-                    }
+                if(found == false) {
+                    throw new Error("Firebase Admin is being goofy again");
                 }
             }
         }
