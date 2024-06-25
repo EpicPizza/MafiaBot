5b7db4646e112dfc6a9617382b66aa71bf63cd67
Alejandro Estrella <aqealejandro@gmail.com>
2024-06-24 18:58:43 -0700

    stats fix

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -83,7 +83,7 @@ async function handleStatsList(interaction: ChatInputCommandInteraction | Comman
     list = list.sort((a, b) => b.messages - a.messages);
     list = list.filter(stat => game.signups.includes(stat.id));
 
-    const id = (await db.collection('graphs').add({ stats: list, day: global.day, name: game.name, timestamp: interaction.type == 'text' ? interaction.message.createdAt : interaction.createdAt.valueOf() })).id;
+    const id = (await db.collection('graphs').add({ stats: list, day: global.day, name: game.name, timestamp: interaction.type == 'text' ? interaction.message.createdAt.valueOf() : interaction.createdAt.valueOf() })).id;
 
     const message = list.reduce((previous, current) => previous += current.name + " » " + current.messages + " message" + (current.messages== 1 ? "" : "s") + " containing " + current.words + " word" + (current.words== 1 ? "" : "s") + "\n", "");
 
