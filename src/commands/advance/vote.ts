5db318ea80ebbda940082eccb27dabc7d426517c
Alejandro Estrella <aqealejandro@gmail.com>
2025-12-21 12:43:51 -0800

    adv vote fix

--- src/commands/advance/vote.ts
+++ src/commands/advance/vote.ts
@@ -69,12 +69,12 @@ export const VoteCommand = {
         if(playerInput == null) throw new Error("Choose a player.");
         const playerUser = await getUserByName(playerInput);
         if(!playerUser) throw new Error("Player not found.");
-        const player = global.players.find(player => player.id == playerUser.id);
+        const player = game.signups.find(player => player == playerUser.id);
         if(!player) throw new Error("Player is not in this game");
 
         const forInput = interaction.type == 'text' ? (interaction.program.args.length > 2 ? interaction.program.processedArgs[2] as string : null) : interaction.options.getString('for');
         const forUser = forInput ? await getUserByName(forInput) : undefined;
-        const forPlayer = forUser ? global.players.find(player => player.id == forUser.id) : undefined;
+        const forPlayer = forUser ? game.signups.find(player => player == forUser.id) : undefined;
 
         const advType = interaction.type == 'text' ? interaction.program.processedArgs[1] as string : interaction.options.getString('type');
         if(advType == null) throw new Error("Vote type not specified.");
