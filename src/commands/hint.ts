ce6160ec7aa7f861ec60aa4c60745c5959e34500
s-gobo <144197685+s-gobo@users.noreply.github.com>
2026-03-30 02:19:32 -0700

    Refactor hint generation logic in hint.ts

--- src/commands/hint.ts
+++ src/commands/hint.ts
@@ -49,15 +49,66 @@ module.exports = {
 
         if(user == null) throw new Error("User not found.");
         
-        let use2POV = getRandom(1, 11) >= 7; // 70%, the chance for using second person
-        let useID = getRandom(1, 11) >= 1 // 10%, the chance of using an identity hint
-        let sentence = use2POV ? "You": user.nickname; // the subject of the sentence
-        sentence += useID ? use2POV ? " are " : " is " : "";
-        let ids = ["alive", "dead", "mafia", "town", "vigilante", "doctor", "cop", "jester", "cooked", "favored by mod"];
-        let verbs = ["will be hammered", "will meow", "will be killed", "will be healed", "will be voted out"];
-        sentence += useID ? ids[getRandom(0, ids.length)] : verbs[getRandom(0, verbs.length)];
-        sentence += ".";
-        return await interaction.reply(sentence);
+        
+        let use2POV = getRandom(1, 11) >= 7; // 30%, the chance for using second person
+        let hints : [string, number, string, number?][] = [ // typescript pmo
+            ["alive", 10, "is"],
+            ["dead", 10, "is"],
+            ["mafia", 10, "is"],
+            ["town", 10, "is"],
+            ["vigilante", 10, "is"],
+            ["doctor", 10, "is"],
+            ["jester", 10, "is"],
+            ["cooked", 10, "is"],
+            ["cooking", 5, "is"],
+            ["throwing", 5, "is"],
+            ["wrong", 8, "is"],
+            ["lying", 8, "is"],
+            ["telling the truth", 15, "is"],
+            ["not Snek", 1, "is"],
+            ["being rooted for by mod", 1, "is"],
+            
+            ["will be hammered", 10, ""],
+            ["will meow", 3, ""],
+            ["will die soon", 2, ""],
+            ["will be protected", 2, ""],
+            ["will be voted out", 2, ""],
+            ["got the good role", 1, ""],
+            
+            
+            ["doomed", 2, "was"],
+            ["meowing", 2, "was"],
+            
+            ["meowed", 10, "had"],
+            ["won", 2, "had"],
+            ["lost", 2, "had"],
+            
+            ["understand typescript", 1, "doesn't"],
+        ];
+        
+        let sum = 0;
+        for (let hint of hints) {
+            sum += hint[1];
+            hint.push(sum);
+        }
+        
+        let random = getRandom(0, sum);
+        
+        for (let hint of hints) {
+            if ((hint[3] || 0) < random) { // typescript pmo
+                return await interaction.reply(
+                    (use2POV ? "You " + {
+                        is: "are",
+                        "": "",
+                        was: "were",
+                        had: "have",
+                        "doesn't": "don't",
+                    }[hint[2]]: user.nickname + " " + hint[2]) + hint[0] + "."
+                )
+            }
+        }
+        
+        return await interaction.reply("Snek's program has problems.");
     }
 }
 
