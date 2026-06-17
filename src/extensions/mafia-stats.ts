094c9aad96fae0e89f62aa7664d3bc1bbfebc660
Alejandro Estrella <aqealejandro@gmail.com>
2026-06-16 20:42:32 -0500

    query fix pt.2

--- src/extensions/mafia-stats.ts
+++ src/extensions/mafia-stats.ts
@@ -85,8 +85,7 @@ module.exports = {
 
         const gameSetup = await getGameSetup(await getGameByID(instance.global.game ?? "---", instance), instance.setup);
 
-        const messages = (await db.collection('channels').doc(gameSetup.mafia.id).collection('messages').startAfter('createdTimestamp', timestamp).get()).docs.map(doc => doc.data() as TrackedMessage);
-        messages.sort((a, b) => a.createdTimestamp - b.createdTimestamp)
+        const messages = (await db.collection('channels').doc(gameSetup.mafia.id).collection('messages').orderBy('createdTimestamp').startAfter(timestamp).get()).docs.map(doc => doc.data() as TrackedMessage);
         const appendStats = messages.map(message => {
             return {
                 messages: 1,
